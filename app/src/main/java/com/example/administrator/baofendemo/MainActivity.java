d6ae17def9649ec30e8c017ba53418b90fa9810e
chailei9005 <chailei9005@163.com>
2016-02-28 17:31:01 +0800

    first commit

---
+++ app/src/main/java/com/example/administrator/baofendemo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.administrator.baofendemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import bf.cloud.android.playutils.LivePlayer;
+//import bf.cloud.android.base.;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
